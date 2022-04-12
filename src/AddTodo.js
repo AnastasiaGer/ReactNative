957ecc8614ba05e39eb58a7b1f139fa98f7a5ab9
anisa <aagermanova@gmail.com>
2022-04-11 19:55:40 -0700

    Add Todo

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View, StyleSheet, TextInput, Button } from 'react-native'
 
-export const AddTodo = props => {
+export const AddTodo = ({ onSubmit }) => {
+  const [value, setValue] = useState('')
+  const pressHandler = () => {
+    if(value.trim()) {
+      onSubmit(value)
+      setValue('')
+    } else {
+      //error
+    }
+  }
+
   return (
     <View style={styles.block}>
-      <TextInput style={styles.input} />
-      <Button title='Добавить' />
+      <TextInput style={styles.input}
+      onChangeText={text => setValue(text)}
+      value={value}
+      placeholder='Add todo'/>
+      <Button title='Add' onPress={pressHandler} />
     </View>
   )
 }
@@ -14,7 +27,8 @@ const styles = StyleSheet.create({
   block: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 15
   },
   input: {
     width: '70%',
