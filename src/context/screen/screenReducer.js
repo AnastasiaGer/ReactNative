b2c69c89c6ac7eb289ce160c9969205007b26de4
anisa <aagermanova@gmail.com>
2022-04-17 15:21:21 -0700

    final

--- src/context/screen/screenReducer.js
+++ src/context/screen/screenReducer.js
@@ -1,10 +1,11 @@
 import { CHANGE_SCREEN } from '../types'
 
+const handlers = {
+  [CHANGE_SCREEN]: (state, payload) => payload,
+  DEFAULT: state => state
+}
+
 export const screenReducer = (state, action) => {
-  switch (action.type) {
-    case CHANGE_SCREEN:
-      return action.payload
-    default:
-      return state
-  }
+  const handler = handlers[action.type] || handlers.DEFAULT
+  return handler(state, action.payload)
 }
--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,19 +1,40 @@
-import React, { useReducer, useContext } from "react";
-import {Alert} from 'react-native'
-import { TodoContext } from "./todoContext";
-import { todoReducer } from "./todoReducer";
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "../types";
-import {ScreenContext} from '../screen/screenContext'
+import React, { useReducer, useContext } from 'react'
+import { Alert } from 'react-native'
+import { TodoContext } from './todoContext'
+import { todoReducer } from './todoReducer'
+import {
+  ADD_TODO,
+  REMOVE_TODO,
+  UPDATE_TODO,
+  SHOW_LOADER,
+  HIDE_LOADER,
+  SHOW_ERROR,
+  CLEAR_ERROR,
+  FETCH_TODOS
+} from '../types'
+import { ScreenContext } from '../screen/screenContext'
 
 export const TodoState = ({ children }) => {
   const initialState = {
-    todos: [{ id: "1", title: "Выучить React Native" }],
-  };
+    todos: [],
+    loading: false,
+    error: null
+  }
+  const { changeScreen } = useContext(ScreenContext)
+  const [state, dispatch] = useReducer(todoReducer, initialState)
 
-  const { changeScreen } = useContext(ScreenContext);
-  const [state, dispatch] = useReducer(todoReducer, initialState);
-
-  const addTodo = (title) => dispatch({ type: ADD_TODO, title });
+  const addTodo = async title => {
+    const response = await fetch(
+      'https://reactnative-b504b-default-rtdb.firebaseio.com/todos.json',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title })
+      }
+    )
+    const data = await response.json()
+    dispatch({ type: ADD_TODO, title, id: data.name })
+  }
 
   const removeTodo = id => {
     const todo = state.todos.find(t => t.id === id)
@@ -28,8 +49,15 @@ export const TodoState = ({ children }) => {
         {
           text: 'Удалить',
           style: 'destructive',
-          onPress: () => {
+          onPress: async () => {
             changeScreen(null)
+            await fetch(
+              `https://reactnative-b504b-default-rtdb.firebaseio.com/todos/${id}.json`,
+              {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' }
+              }
+            )
             dispatch({ type: REMOVE_TODO, id })
           }
         }
@@ -38,18 +66,68 @@ export const TodoState = ({ children }) => {
     )
   }
 
-  const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title });
+  const fetchTodos = async () => {
+    showLoader()
+    clearError()
+    try {
+      const response = await fetch(
+        'https://reactnative-b504b-default-rtdb.firebaseio.com/todos.json',
+        {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' }
+        }
+      )
+      const data = await response.json()
+      if(data === null) {
+        return initialState.todos
+      }
+      // console.log('Fetch data', data)
+      const todos = Object.keys(data).map(key => ({ ...data[key], id: key }))
+      dispatch({ type: FETCH_TODOS, todos })
+    } catch (e) {
+      showError('Что-пошло не так...')
+      console.log(e)
+    } finally {
+      hideLoader()
+    }
+  }
+
+  const updateTodo = async (id, title) => {
+    clearError()
+    try {
+      await fetch(`https://reactnative-b504b-default-rtdb.firebaseio.com/todos/${id}.json`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title })
+      })
+      dispatch({ type: UPDATE_TODO, id, title })
+    } catch (e) {
+      showError('Что-пошло не так...')
+      console.log(e)
+    }
+  }
+
+  const showLoader = () => dispatch({ type: SHOW_LOADER })
+
+  const hideLoader = () => dispatch({ type: HIDE_LOADER })
+
+  const showError = error => dispatch({ type: SHOW_ERROR, error })
+
+  const clearError = () => dispatch({ type: CLEAR_ERROR })
 
   return (
     <TodoContext.Provider
       value={{
         todos: state.todos,
+        loading: state.loading,
+        error: state.error,
         addTodo,
         removeTodo,
         updateTodo,
+        fetchTodos
       }}
     >
       {children}
     </TodoContext.Provider>
-  );
-};
+  )
+}
